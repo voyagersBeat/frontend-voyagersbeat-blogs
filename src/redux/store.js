65e7260910e0618d67a458082fe5a4afb9ba6c14
priyanshu-inu <priyanshuinuu@gmail.com>
2024-12-23 00:03:35 +0530

    first commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { blogApi } from "./features/blog/blogApi";
-import authApi from "./features/auth/AuthApi";
-import authReducer from "./features/auth/authSlice";
-import commentApi from "./features/comment/commentApi";
+import { blogApi } from "./features/blog/blogApi.js";
+import authApi from "./features/auth/AuthApi.js";
+import authReducer from "./features/auth/authSlice.js";
+import commentApi from "./features/comment/commentApi.js";
 
 export const store = configureStore({
   reducer: {
