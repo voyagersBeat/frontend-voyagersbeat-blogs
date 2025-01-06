bdfc45d98a4ae27c4c85d280b5d5c4a54d29812b
priyanshu-inu <priyanshuinuu@gmail.com>
2025-01-06 13:57:43 +0530

    first commit

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -1,16 +1,23 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { FiUsers } from "react-icons/fi";
+import { FaBlog, FaRegComment } from "react-icons/fa";
+import { RiAdminLine } from "react-icons/ri";
+import { useFetchBlogsQuery } from "../../../redux/features/blog/blogApi";
+import { useGetCommentsQuery } from "../../../redux/features/comment/commentApi";
+import { useGetUserQuery } from "../../../redux/features/auth/AuthApi";
+import BlogGraphChart from "./BlogGraphChart";
+
 const Dashboard = () => {
   const [query, setQuery] = useState({ search: "", category: "" });
   const { user } = useSelector((state) => state.auth);
   const { data: blogs = [], error, isLoading } = useFetchBlogsQuery(query);
-  const { data: comments = { totalComments: 0 } } = useGetCommentsQuery();
-  const { data: users = { users: [] } } = useGetUserQuery(); // Default structure
-
-  // Safely calculate admin count
-  const adminCount =
-    users?.users?.filter((user) => user.role === "admin").length || 0;
-
-  console.log("Admin count:", adminCount);
-
+  const { data: comments = [] } = useGetCommentsQuery();
+  const { data: users = [] } = useGetUserQuery();
+  const adminCount = users.users?.filter(
+    (user) => user.role === "admin"
+  ).length;
+  console.log(adminCount);
   return (
     <>
       {isLoading && <div>Loading...</div>}
@@ -24,11 +31,11 @@ const Dashboard = () => {
         <div className="flex flex-col md:flex-row justify-between gap-8 pt-8">
           <div className="bg-indigo-100 py-5 w-full rounded-sm space-y-1 flex flex-col items-center">
             <FiUsers className="size-8 text-indigo-600" />
-            <p>{users.users?.length || 0} Users</p>
+            <p> 199 Users</p>
           </div>
           <div className="bg-red-100 py-5 w-full rounded-sm space-y-1 flex flex-col items-center">
             <FaBlog className="size-8 text-red-600" />
-            <p>{blogs.length} Blogs</p>
+            <p> {blogs.length} Blogs</p>
           </div>
           <div className="bg-lime-100 py-5 w-full rounded-sm space-y-1 flex flex-col items-center">
             <RiAdminLine className="size-8 text-lime-600" />
@@ -38,10 +45,10 @@ const Dashboard = () => {
           </div>
           <div className="bg-orange-100 py-5 w-full rounded-sm space-y-1 flex flex-col items-center">
             <FaRegComment className="size-8 text-orange-600" />
-            <p>{comments.totalComments || 0} Comment</p>
+            <p> {comments.totalComments} Comment</p>
           </div>
         </div>
-        {/* graph section */}
+        {/* graph section  */}
         <div className="pt-5 pb-5">
           <BlogGraphChart blogs={blogs} />
         </div>
